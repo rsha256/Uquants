1ed2c9be8e3da49b8f4f4811d9c6528f9c7aeb17
Rahul Shah <rahuldshah3@gmail.com>
2018-07-03 18:11:06 -0400

    converted some stuff from php to html bc the site will be static

--- js/init.js
+++ js/init.js
@@ -1,8 +1,24 @@
-(function($){
-  $(function(){
+(function ($) {
+  $(function () {
 
     $('.sidenav').sidenav();
     $('.parallax').parallax();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+// start
+$(document).ready(function () {
+
+  // Dropdown
+  $('.dropdown-trigger').dropdown({
+      closeOnClick:true,
+      coverTrigger: false,
+      inDuration: 300,
+      outDuration: 225,
+      hover: true, // Activate on hover
+      belowOrigin: false // Displays dropdown below the button
+    }
+  );
+
+});
